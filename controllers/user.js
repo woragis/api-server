f9877cf0ac1f51bc44fcc808cc47d59f788b50dc
woragis <masteringthecode.woragis+github@gmail.com>
2023-09-24 00:09:40 -0300

    Set up user controller postgres config

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+const { Pool } = require("pg");
+
+const userTable = process.env.POSTGRES_DATABASE_TABLE_ACCOUNT;
+const pool = new Pool({
+  host: process.env.POSTGRES_DATABASE_HOST,
+  port: process.env.POSTGRES_DATABASE_PORT,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_DATABASE_USER,
+  password: process.env.POSTGRES_DATABASE_USER_PASSWORD,
+});
