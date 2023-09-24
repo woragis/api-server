bdd674c64dc575efc497b88ea47b417bc5497b4e
woragis <masteringthecode.woragis+github@gmail.com>
2023-09-24 00:12:32 -0300

    Set up user route config

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const { createAccount, loginUser } = require("../controllers/user");
+
+router.route("/register").post(createAccount);
+router.route("/login").post(loginUser);
+
 module.exports = router;
