adc3cab5fb78d076fb03d9d6ebda2d7697f51f8c
woragis <masteringthecode.woragis+github@gmail.com>
2023-09-24 00:08:07 -0300

    Init server file

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const server_port = process.env.EXPRESS_SERVER_PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(server_port, () => {
+  console.log(`Server Running on port ${server_port}`);
+});
