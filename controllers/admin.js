c19dd8f31c54dff849598c8043304eb4cdcd789c
woragis <masteringthecode.woragis+github@gmail.com>
2023-09-26 15:18:05 -0300

    Decrease callbacks in controllers and used client to query in database
    Add client to query in database and decreased callbacks a little

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
-const { Pool } = require("pg");
+const { Client, Pool } = require("pg");
 
-const userTable = process.env.POSTGRES_DATABASE_TABLE_ACCOUNT;
+const client = new Client({
+  host: process.env.POSTGRES_DATABASE_HOST,
+  port: process.env.POSTGRES_DATABASE_PORT,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_DATABASE_USER,
+  password: process.env.POSTGRES_DATABASE_USER_PASSWORD,
+});
+
 const pool = new Pool({
   host: process.env.POSTGRES_DATABASE_HOST,
   port: process.env.POSTGRES_DATABASE_PORT,
@@ -11,6 +18,7 @@ const pool = new Pool({
   password: process.env.POSTGRES_DATABASE_USER_PASSWORD,
 });
 
+const userTable = process.env.POSTGRES_DATABASE_TABLE_ACCOUNT;
 // get all users
 const getUsersQuery = `SELECT * FROM ${userTable};`;
 // get single user
@@ -32,12 +40,8 @@ const salt = bcrypt.genSaltSync(10);
 const getUsers = async (req, res) => {
   const client = await pool.connect();
   try {
-    client.query(getUsersQuery, (err, result) => {
-      if (err) {
-        res.status(500).json({ message: "Internal server error" });
-      }
-      res.status(200).json(result.rows);
-    });
+    const result = client.query(getUserQuery);
+    res.status(200).json(result.rows);
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   } finally {
@@ -47,37 +51,25 @@ const getUsers = async (req, res) => {
 
 const registerUser = async (req, res) => {
   const { email, password } = req.body;
-
-  const client = await pool.connect();
-
   try {
-    client.query(checkIfEmailExists, [email], (err, result) => {
-      if (err) res.locals.serverError;
-
-      const { email_exists } = result.rows[0];
-      if (email_exists) {
-        res.status(400).json({ message: "email already exists, try again" });
-      } else {
-        const hashedPassword = bcrypt.hashSync(password, salt);
-        client.query(
-          registerUserInDB,
-          [email, hashedPassword],
-          (err, result) => {
-            if (err) res.locals.serverError;
-            res.status(201).json([
-              { message: "user created" },
-              {
-                email: email,
-                password: password,
-                hashedPassword: hashedPassword,
-              },
-            ]);
-          }
-        );
-      }
-    });
+    await client.connect();
+    const checkEmail = await client.query(checkIfEmailExists, [email]);
+    const { email_exists } = checkEmail.rows[0];
+    if (!email_exists) {
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      client.query(registerUserInDB, [email, hashedPassword]);
+      res.status(201).json([
+        { message: "user created" },
+        {
+          email: email,
+          password: hashedPassword,
+        },
+      ]);
+    } else {
+      res.status(400).json({ message: "email already exists, try again" });
+    }
   } catch (err) {
-    res.locals.serverError;
+    res.status(500).json({ message: "Internal server error" });
   } finally {
     client.release();
   }
@@ -87,103 +79,91 @@ const getUser = async (req, res) => {
   const { id } = req.params;
   const client = await pool.connect();
   try {
-    client.query(getUserQuery, [id], (err, result) => {
-      if (err) res.locals.serverError;
-      res.json(result.rows);
-    });
+    const { rows } = await client.query(getUserQuery, [id]);
+    res.status(200).json(rows);
   } catch (err) {
-    res.locals.serverError;
+    res.status(500).json({ message: "Server internal error" });
   } finally {
     client.release();
   }
+  pool.end();
 };
 
 const updateUser = async (req, res) => {
   const { id } = req.params;
   const { email, password } = req.body;
-  const client = await pool.connect();
-  try {
-    if (email) {
-      client.query(checkIfEmailChanged, [id, email], (err, result) => {
-        if (err) res.locals.serverError;
 
-        const { email_changed } = result.rows[0];
-        if (!email_changed) {
-          res.status(400).json({ message: "email is the same" });
-        } else {
-          client.query(
-            checkIfEmailAlreadyExists,
-            [id, email],
-            (err, result) => {
-              const { email_already_exists } = result.rows[0];
-              if (email_already_exists) {
-                res.status(400).json({ message: "email already exists" });
-              } else {
-                if (password) {
-                  console.log("modifying email and password");
-                  const hashedPassword = bcrypt.hashSync(password, salt);
-                  client.query(
-                    updateUserEmailPasswordQuery,
-                    [id, email, hashedPassword],
-                    (err, result) => {
-                      if (err) res.locals.serverError;
-                      else {
-                        res
-                          .status(202)
-                          .json([
-                            { message: `Updated user ${id}` },
-                            result.rows,
-                          ]);
-                      }
-                    }
-                  );
-                } else {
-                  client.query(
-                    updateUserEmailQuery,
-                    [id, email],
-                    (err, result) => {
-                      if (err) res.locals.serverError;
-                      res
-                        .status(202)
-                        .json([{ message: `Updated user ${id}` }, result.rows]);
-                    }
-                  );
-                }
+  try {
+    await client.connect();
+    if (email) {
+      const resultCheckIfEmailChanged = await client.query(
+        checkIfEmailChanged,
+        [id, email]
+      );
+      const { email_changed } = resultCheckIfEmailChanged.rows[0];
+      if (email_changed) {
+        const resultCheckIfEmailAlreadyExists = await client.query(
+          checkIfEmailAlreadyExists,
+          [id, email]
+        );
+        const { email_already_exists } =
+          resultCheckIfEmailAlreadyExists.rows[0];
+        if (!email_already_exists) {
+          if (password) {
+            const hashedPassword = bcrypt.hashSync(password, salt);
+            client.query(
+              updateUserEmailPasswordQuery,
+              [id, email, hashedPassword],
+              (err, result) => {
+                res
+                  .status(202)
+                  .json([{ message: `Updated user ${id}` }, result.rows]);
               }
-            }
-          );
+            );
+          } else {
+            client.query(updateUserEmailQuery, [id, email], (err, result) => {
+              res
+                .status(202)
+                .json([{ message: `Updated user ${id}` }, result.rows]);
+            });
+          }
+        } else if (email_already_exists) {
+          res.status(400).json({ message: "email already exists" });
         }
-      });
+      } else if (!email_changed) {
+        res.status(400).json({ message: "email is the same" });
+      }
     } else if (password) {
       const hashedPassword = bcrypt.hashSync(password, salt);
       client.query(
         updateUserPasswordQuery,
         [id, hashedPassword],
         (err, result) => {
-          res.status(202).json(result.rows);
+          res
+            .status(202)
+            .json([{ message: `Updated user ${id}` }, result.rows]);
         }
       );
     } else {
       res.status(400).json({ message: "Provide values" });
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Internal server error" });
   } finally {
-    client.release();
+    await client.end();
   }
 };
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
-  const client = await pool.connect();
   try {
-    client.query(deleteUserQuery, [id], (err, result) => {
-      res.status(200).json({ message: "User deleted" });
-    });
+    await client.connect();
+    await client.query(deleteUserQuery, [id]);
+    res.status(200).json({ message: "User deleted" });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "Server internal error" });
   } finally {
-    client.release();
+    await client.end();
   }
 };
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
-const { Pool } = require("pg");
+const { Client } = require("pg");
 
-const userTable = process.env.POSTGRES_DATABASE_TABLE_ACCOUNT;
-const pool = new Pool({
+const client = new Client({
   host: process.env.POSTGRES_DATABASE_HOST,
   port: process.env.POSTGRES_DATABASE_PORT,
   database: process.env.POSTGRES_DATABASE,
@@ -11,76 +10,75 @@ const pool = new Pool({
   password: process.env.POSTGRES_DATABASE_USER_PASSWORD,
 });
 
+const userTable = process.env.POSTGRES_DATABASE_TABLE_ACCOUNT;
 // database queries:
 const checkIfEmailExists = `SELECT EXISTS (SELECT 1 FROM ${userTable} WHERE email=$1) as email_exists;`;
 const getPassword = `SELECT password FROM ${userTable} WHERE email=$1;`;
-
+// create user :)
 const createUserInDB = `INSERT INTO ${userTable} (email, password) VALUES ($1, $2)`;
 
-const saltRounds = 10;
+const salt = bcrypt.genSalt(10);
 
 const registerUser = async (req, res) => {
   const { email, password } = req.body;
-  const client = await pool.connect();
 
   try {
-    client.query(checkIfEmailExists, [email], (err, result) => {
-      if (err) res.status(500).json({ message: "Internal server error" });
-      const { email_exists } = result.rows[0];
-      if (email_exists) {
-        res.status(400).json({ message: "email already exists, try again" });
-      } else {
-        const hashedPassword = bcrypt.hashSync(password, saltRounds);
-        client.query(createUserInDB, [email, hashedPassword], (err, result) => {
-          if (err) res.status(500).json({ message: "Internal server error" });
-          res
-            .status(201)
-            .json([
-              { message: "created user" },
-              { email: email, password: password },
-            ]);
-        });
-      }
-    });
+    await client.connect();
+
+    const resultCheckIfEmailExists = await client.query(checkIfEmailExists, [
+      email,
+    ]);
+    const { email_exists } = resultCheckIfEmailExists.rows[0];
+    if (!email_exists) {
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      await client.query(createUserInDB, [email, hashedPassword]);
+      res
+        .status(201)
+        .json([
+          { message: "created user" },
+          { email: email, password: password },
+        ]);
+    } else if (email_exists) {
+      res.status(400).json({ message: "email already exists, try again" });
+    }
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   } finally {
-    client.release();
+    await client.end();
   }
 };
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const client = await pool.connect();
-
   try {
-    client.query(checkIfEmailExists, [email], (err, result) => {
-      if (err) res.status(400).json({ error: "badRequest" });
-      const { email_exists } = result.rows[0];
-      if (!email_exists) {
-        res.status(400).json({ message: "wrong/invalid email" });
-      } else {
-        client.query(getPassword, [email], (err, result) => {
-          const dbPassword = result.rows[0].password;
-          const isPasswordCorrect = bcrypt.compareSync(password, dbPassword);
-          if (!isPasswordCorrect) {
-            res.status(400).json({ message: "wrong/invalid password" });
-          } else {
-            res
-              .status(200)
-              .json([
-                { message: "logged in" },
-                { email: email, password: password },
-              ]);
-          }
-        });
+    await client.connect();
+
+    const resultCheckIfEmailExists = await client.query(checkIfEmailExists, [
+      email,
+    ]);
+    const { email_exists } = resultCheckIfEmailExists.rows[0];
+    if (email_exists) {
+      const resultDBPassword = await client.query(getPassword, [email]);
+      const dbPassword = resultDBPassword.rows[0].password;
+      const isPasswordCorrect = bcrypt.compareSync(password, dbPassword);
+      if (isPasswordCorrect) {
+        res
+          .status(200)
+          .json([
+            { message: "logged in" },
+            { email: email, password: password },
+          ]);
+      } else if (!isPasswordCorrect) {
+        res.status(400).json({ message: "wrong/invalid password" });
       }
-    });
+    } else if (!email_exists) {
+      res.status(400).json({ message: "wrong/invalid email" });
+    }
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   } finally {
-    client.release();
+    await client.end();
   }
 };
 
